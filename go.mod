b5c9008beef8d65dc834d2a9bb37ec9d4edd5d57
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2025-07-18 00:27:06 +0600

    Basic blockchain prototype

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module Blockchain
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"Blockchain/controllers"
+	"fmt"
+)
+
+func main() {
+	bc := controllers.NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to Iliias")
+	bc.AddBlock("Send 2 more BTC to Iliias")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
