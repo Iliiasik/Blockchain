0609373f72d586fbddec439aceb1a71fd2fd07f4
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2025-07-18 16:10:43 +0600

    Proof-of-work. Connect BoltDB, basic cli

--- main.go
+++ main.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"Blockchain/controllers"
-	"fmt"
 )
 
 func main() {
 	bc := controllers.NewBlockchain()
+	defer bc.Db.Close()
 
-	bc.AddBlock("Send 1 BTC to Iliias")
-	bc.AddBlock("Send 2 more BTC to Iliias")
-
-	for _, block := range bc.Blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
+	cli := controllers.CLI{Bc: bc}
+	cli.Run()
 }
